493d2e809065af5811b49919adfb124ac7985f5c
Junior Ricardo <junior.r.huamani@gmail.com>
2020-12-18 15:06:00 -0300

    Agregar archivos de proyecto.

---
+++ AsyncBreakfast/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AsyncBreakfast
+{
+    public static class Helper
+    {
+        public static void StartTime(Stopwatch stopwatch)
+        {
+            stopwatch.Start();
+        }
+
+        public static void StopTimeAndView(Stopwatch stopWatch)
+        {
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            var ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            var elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
---
+++ AsyncBreakfast/Model.cs
@@ -0,0 +1,25 @@
+﻿namespace AsyncBreakfast
+{
+    public class Model
+    {
+        internal class Juice
+        {
+        }
+
+        internal class Toast
+        {
+        }
+
+        internal class Bacon
+        {
+        }
+
+        internal class Egg
+        {
+        }
+
+        internal class Coffee
+        {
+        }
+    }
+}
