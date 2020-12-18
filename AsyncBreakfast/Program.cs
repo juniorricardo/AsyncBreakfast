81b62f9558b06428ad03480e65d81b9f0f7da63d
Junior Ricardo <junior.r.huamani@gmail.com>
2020-12-18 16:05:32 -0300

    Uso de await para evitar los bloqueos

--- AsyncBreakfast/Program.cs
+++ AsyncBreakfast/Program.cs
@@ -7,26 +7,26 @@ namespace AsyncBreakfast
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             var stopWatch = new Stopwatch();
             Helper.StartTime(stopWatch);
 
-            var cup = PourCoffee();
+            Coffee cup = PourCoffee();
             Console.WriteLine("coffee is ready");
 
-            var eggs = FryEggs(2);
+            Egg eggs = await FryEggsAsync(2);
             Console.WriteLine("eggs are ready");
 
-            var bacon = FryBacon(3);
+            Bacon bacon = await FryBaconAsync(3);
             Console.WriteLine("bacon is ready");
 
-            var toast = ToastBread(2);
+            Toast toast = await ToastBreadAsync(2);
             ApplyButter(toast);
             ApplyJam(toast);
             Console.WriteLine("toast is ready");
 
-            var oj = PourOJ();
+            Juice oj = PourOJ();
             Console.WriteLine("oj is ready");
             Console.WriteLine("Breakfast is ready!");
 
@@ -49,30 +49,30 @@ namespace AsyncBreakfast
             Console.WriteLine("Putting butter on the toast");
         }
 
-        private static Toast ToastBread(int slices)
+        private static Task<Toast> ToastBreadAsync(int slices)
         {
             for (var slice = 0; slice < slices; slice++) Console.WriteLine("Putting a slice of bread in the toaster");
             Console.WriteLine("Start toasting...");
             Task.Delay(3000).Wait();
             Console.WriteLine("Remove toast from toaster");
 
-            return new Toast();
+            return Task.FromResult(new Toast());
         }
 
-        private static Bacon FryBacon(int slices)
+        private static Task<Bacon> FryBaconAsync(int slices)
         {
             Console.WriteLine($"putting {slices} slices of bacon in the pan");
             Console.WriteLine("cooking first side of bacon...");
-            Task.Delay(3000).Wait();
+            Task.Delay(3000);
             for (var slice = 0; slice < slices; slice++) Console.WriteLine("flipping a slice of bacon");
             Console.WriteLine("cooking the second side of bacon...");
             Task.Delay(3000).Wait();
             Console.WriteLine("Put bacon on plate");
 
-            return new Bacon();
+            return Task.FromResult(new Bacon());
         }
 
-        private static Egg FryEggs(int howMany)
+        private static Task<Egg> FryEggsAsync(int howMany)
         {
             Console.WriteLine("Warming the egg pan...");
             Task.Delay(3000).Wait();
@@ -81,7 +81,7 @@ namespace AsyncBreakfast
             Task.Delay(3000).Wait();
             Console.WriteLine("Put eggs on plate");
 
-            return new Egg();
+            return Task.FromResult(new Egg());
         }
 
         private static Coffee PourCoffee()
